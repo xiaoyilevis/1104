bdd4d3ab8ea40506c9e89fe43ae7072a56a4e774
xiaoyilevis <55223747+xiaoyilevis@users.noreply.github.com>
2020-11-11 15:01:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+let 開始時間 = 0
+let 結束時間 = 0
 basic.forever(function () {
-	
+    // 白線上
+    // 停下來
+    if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.stop(BBStopMode.Brake)
+        bitbot.rotate(BBRobotDirection.Right, 30)
+        開始時間 = input.runningTime()
+        while (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 1 && input.runningTime() - 開始時間 <= 400 || input.runningTime() - 開始時間 > 400) {
+            bitbot.rotate(BBRobotDirection.Right, 30)
+        }
+        結束時間 = input.runningTime() - 開始時間
+    }
+    // 右邊
+    if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
+        bitbot.rotate(BBRobotDirection.Left, 15)
+    }
+    // 左邊
+    if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
+        bitbot.rotate(BBRobotDirection.Right, 115)
+    }
+    // 黑線上
+    if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 1) {
+        bitbot.go(BBDirection.Forward, 60)
+    }
 })
